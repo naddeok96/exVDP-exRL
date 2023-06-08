4e09707a50313b9850b0a2413052e3e2dafd3c93
naddeok96 <naddeok5@students.rowan.edu>
2023-06-07 20:55:03 -0400

    m:

--- dqn.py
+++ dqn.py
@@ -91,5 +91,5 @@ class DQNAgent:
         
     def load_model(self, filename):
         self.model.load_state_dict(torch.load(filename))
-        self.model.eval()
+        self.model.to(self.device)
         self.update_target_model()
